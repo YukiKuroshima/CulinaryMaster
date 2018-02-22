a0b31a0b5a903a245e3448de976e0c7c3297c00b
YukiKuroshima <yukikuro1993@gmail.com>
2018-02-22 15:49:11 -0800

    :tada: Config Travis CI

--- server/api/auth/views.py
+++ server/api/auth/views.py
@@ -19,6 +19,7 @@ def ping_pong():
 def login():
     return render_template('login.html')
 
+
 @auth_blueprint.route('/signup', methods=['GET'])
 def signup():
     return render_template('signup.html')
