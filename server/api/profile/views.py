5e5c1406815ca85b3854b7dbd6b9695cf6c201a9
Duoc Nguyen <duocnguyen1996@gmail.com>
2018-03-12 16:24:57 -0700

    still need data persistent on edit profile

--- server/api/profile/views.py
+++ server/api/profile/views.py
@@ -1,14 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 from flask_login import login_required
 from server.api.profile.form import EditProfileForm
+import json
 
 profile_blueprint = Blueprint('profile', __name__, template_folder='./templates')
 
+data = {}
+data['name'] = 'Duoc Nguyen'
+data['image'] = 'http://www.personalbrandingblog.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640-300x300.png'
+json_data = json.dumps(data)
 
 @profile_blueprint.route('/profile', methods=['GET'])
 @login_required
 def profile():
-    return render_template('profile.html')
+    return render_template('profile.html', data=data)
 
 @profile_blueprint.route('/profile/edit', methods=['GET', 'POST'])
 @login_required
@@ -17,11 +22,11 @@ def edit_profile():
 	if form.validate_on_submit():
 		name = form.name.data
 		image = form.imageURL.data
-		data = {name: name, image: image}
 		if name is None:
 			form.name.errors.append('Invalid Name')
 		elif image is None:
 			form.image.errors.append('Invalid image url')
 		else:
-			return redirect("/profile", data)
+			data[]
+			return redirect("/profile")
 	return render_template('edit.html', form=form)
