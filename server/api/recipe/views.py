7ccce9e14db64f70469ca2cdc94719d893b89207
Duoc Nguyen <duocnguyen1996@gmail.com>
2018-05-01 12:05:52 -0700

    changed recipe layout

--- server/api/recipe/views.py
+++ server/api/recipe/views.py
@@ -8,20 +8,6 @@ from server.api.recipe.img_url import fetch_img_url
 recipe_blueprint = Blueprint('recipe', __name__, template_folder='./templates')
 
 
-data = [{	"name": "Blue Berry Yogurt",
-			"image": "https://images.unsplash.com/photo-1482423064560-3e394ae8f216?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5f129e30860e5f930955ac0591bd57c8&auto=format&fit=crop&w=2250&q=80",
-			"description": "You Have all the ingredients to make this.",
-			"num_ingredients": "5",
-			"id": "1234"
-		},
-		{	"name": "Ham and Egg Sandwich",
-			"image": "https://images.unsplash.com/photo-1479894720049-067d8b87f2d9?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ea23bfdafc40b5d5edcbb22c826ad444&auto=format&fit=crop&w=800&q=60",
-			"description": "You Have all the ingredients to make this.",
-			"num_ingredients": "5",
-			"id": "2345"
-		}
-		]
-
 @login_required
 @recipe_blueprint.route('/recipe/<id>', methods=['GET'])
 def detail_recipe(id=None):
@@ -44,7 +30,7 @@ def recipe():
         recipe_img_url = fetch_img_url(value['title'])
         result[key]['image'] = recipe_img_url
 
-    return render_template('recipe.html', data=data, recipes=result)
+    return render_template('recipe.html', recipes=result)
 
 
 @login_required
